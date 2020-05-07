e0e311f31b6aef5cd288f37a91e5cf5dea2f34f8
Santosh Choudhury <santoshchoudhury5@gmail.com>
2020-05-07 19:55:24 +0530

    #changes

---
+++ Chenges Name/employeeotherdocuments.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 07, 2020 at 04:18 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.3.16
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pms`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employeeotherdocuments`
+--
+
+CREATE TABLE `employeeotherdocuments` (
+  `id` int(11) NOT NULL,
+  `employee_id` varchar(20) DEFAULT NULL,
+  `documentname` varchar(50) DEFAULT NULL,
+  `document` varchar(1000) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `employeeotherdocuments`
+--
+
+INSERT INTO `employeeotherdocuments` (`id`, `employee_id`, `documentname`, `document`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, '20', 'ff', '15888575681569302125eb40ae0efdf1logo1.png', '2020-05-07 13:19:28', '2020-05-07 13:19:28', NULL),
+(2, '20', 'ee', '15888585607658670855eb40ec0b259alogo1.png', '2020-05-07 13:36:00', '2020-05-07 14:09:48', '2020-05-07 14:09:48');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `employeeotherdocuments`
+--
+ALTER TABLE `employeeotherdocuments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `employeeotherdocuments`
+--
+ALTER TABLE `employeeotherdocuments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
