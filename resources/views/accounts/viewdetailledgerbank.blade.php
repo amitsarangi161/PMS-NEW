3b1c0f8c9b0b74d5479bc54e6a64cf34ac0ff57b
Amit <amitsarangi161@gmail.com>
2020-06-02 09:50:41 +0530

    #changes

---
+++ resources/views/accounts/viewdetailledgerbank.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.account')
+@section('content')
+<div class="table-responsive">
+<table class="table table-responsive table-hover table-bordered table-striped sortfalsedatatable">
+	<thead>
+		<tr>
+			<td>ID</td>
+			<td>DATE</td>
+			<td>OB</td>
+			<td>CREDIT</td>
+			<td>DEBIT</td>
+			<td>CB</td>
+		</tr>
+	</thead>
+	<tbody>
+		  @php 
+		     $ob=$ob;
+		  @endphp 
+		@foreach($bankledgers as $bankledger)
+		 
+		 <tr>
+		 <td>{{$bankledger->id}}</td>
+		 <td>{{$bankledger->date}}</td>
+		 <td>{{$ob}}</td>
+		 <td>{{$tcr=$bankledger->sumcr}}</td>
+		 <td>{{$tdr=$bankledger->sumdr}}</td>
+		 <td>{{$ob=($ob+$tcr)-$tdr}}</td>
+		 </tr>
+		@endforeach
+	</tbody>
+
+</table>
+</div>
+@endsection
